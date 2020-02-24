e62618096a569643c27692b356d332ded52dc0bb
Amarjit Singh <amarjitrai1@yahoo.com>
2020-02-24 17:56:42 +0800

    working

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root to: 'games#game'
+  root to: 'games#new'
   get 'new', to: 'games#new'
 
-  get 'score', to: 'games#score'
+  post 'score', to: 'games#score'
 end
